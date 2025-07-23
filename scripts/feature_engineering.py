5de279b4a56d4a571bd7e2fc59318a5b8dffd79d
Jayanth2323 <jayanthchennoju@gmail.com>
2025-07-23 12:19:18 +0530

    Scripts  modified

---
+++ scripts/feature_engineering.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+def create_lag_features(series, window_size=24):
+    X, y = [], []
+    for i in range(len(series) - window_size):
+        X.append(series[i:i + window_size])
+        y.append(series[i + window_size])
+    return pd.DataFrame(X), pd.Series(y)
